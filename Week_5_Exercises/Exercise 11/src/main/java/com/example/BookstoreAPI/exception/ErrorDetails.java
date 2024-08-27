fa78c6797103db0d9ec848c0d6f5b9aead78127d
Syed Akthar Ahmed <132035007+Akthar2850@users.noreply.github.com>
2024-08-28 02:30:32 +0530

    Add files via upload

---
+++ Week_5_Exercises/Exercise 11/src/main/java/com/example/BookstoreAPI/exception/ErrorDetails.java
@@ -0,0 +1,8 @@
+package com.example.BookstoreAPI.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+    public ErrorDetails(Date date, String methodNotAllowed, String description) {
+    }
+}
